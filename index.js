e912672fd6003ac10e8da308b66824f5e335954b
Andrew Jenkins <andrewj@f5.com>
2015-01-23 03:33:22 +0000

    Add a memory cache, info about repository.  Make a helper for top-level.

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ var log = require('winston');
 log.cli();
 log.level = 'debug'
 
+function lcb(cb) {
+  return function (err, result) {
+    if (err) {
+      log.error('Error:', err);
+    }
+    log.info('Result:', result);
+    cb(err, result);
+  };
+}
+
 async.series([
-  function (cb) { pkgInfo.versions('async', cb); },
-  function (cb) { pkgInfo.versions('express', cb); },
+  function (cb) { pkgInfo.versions('async', lcb(cb)); },
+  function (cb) { pkgInfo.versions('express', lcb(cb)); },
+  function (cb) { pkgInfo.repository('express', lcb(cb)); },
+  function (cb) { pkgInfo.repository('async', lcb(cb)); },
 ],
 function (err, result) {
   log.info('done');
