bd85b853c5d88f88bd782383beea7c995cb0e034
Andrew Jenkins <andrewj@f5.com>
2015-01-23 05:04:50 +0000

    Add dependedUpon.js for parsing dependedUpon.json, start logic.
    Added dependedUpon.js.
    
    Started the logic in index.js for getting a list of most depended upon,
    and then getting the package info for those.  Commented out the old
    async.series test code, and started a waterfall.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var pkgInfo = require('./lib/pkgInfo.js');
+var dependedUpon = require('./lib/dependedUpon.js');
 var async = require('async');
 var log = require('winston');
 
 log.cli();
 log.level = 'debug'
+var PACKAGES = 10, VERSIONS = 10;
 
 function lcb(cb) {
   return function (err, result) {
@@ -15,12 +17,51 @@ function lcb(cb) {
   };
 }
 
+function getMostDependedUpon(cb) {
+  dependedUpon.most(PACKAGES, function (err, du) {
+    if (err) return cb(err);
+    return cb(null, du.map(function (val) { return val.name; }));
+  });
+}
+
+function getVersionsForEachPackage(pkgs, cb) {
+  var pkgVers = {};
+
+  var failed;
+  async.each(pkgs,
+    function (pkg, cb) {
+      pkgInfo.versions(pkg, function (err, res) {
+        if (err) return cb(err);
+        log.debug('Adding info for package %s:', pkg, res);
+        pkgVers[pkg] = res;
+        cb();
+      });
+    },
+    function (err) {
+      if (err) return cb(err);
+      return cb(null, pkgVers);
+    }
+  );
+}
+
+
+/*
 async.series([
+  function (cb) { getMostDependedUpon(lcb(cb)); },
   function (cb) { pkgInfo.versions('async', lcb(cb)); },
   function (cb) { pkgInfo.versions('express', lcb(cb)); },
   function (cb) { pkgInfo.repository('express', lcb(cb)); },
   function (cb) { pkgInfo.repository('async', lcb(cb)); },
+  function (cb) { dependedUpon.most(lcb(cb)); },
 ],
 function (err, result) {
   log.info('done');
 });
+*/
+
+async.waterfall([
+  getMostDependedUpon,
+  getVersionsForEachPackage,
+  ],
+  lcb(function () {})
+);
